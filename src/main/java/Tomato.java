00aaecb42c80298065514dea2f3371e4bac53e09
cmitche09 <cmitche09@gmail.com>
2020-09-30 10:24:42 -0400

    add instance variables to Tomato

--- src/main/java/Tomato.java
+++ src/main/java/Tomato.java
@@ -1,3 +1,25 @@
 public class Tomato {
+    private int leaves;
+    private String color;
 
+    public Tomato(int leaves, String color) {
+        this.leaves = leaves;
+        this.color = color;
+    }
+
+    public int getLeaves() {
+        return leaves;
+    }
+
+    public void setLeaves(int leaves) {
+        this.leaves = leaves;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
